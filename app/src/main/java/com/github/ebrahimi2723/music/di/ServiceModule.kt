9a27b2dcf776e0483781922c04da5fe7ad6538b1
mohammad2723 <kotlin2723@gmail.com>
2023-12-12 16:07:59 +0330

    DI completed

--- app/src/main/java/com/github/ebrahimi2723/music/di/ServiceModule.kt
+++ app/src/main/java/com/github/ebrahimi2723/music/di/ServiceModule.kt
@@ -1,37 +1,73 @@
 package com.github.ebrahimi2723.music.di
 
-//import com.google.android.exoplayer2.ExoPlayer
 import android.content.Context
+import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.datasource.cache.NoOpCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.ExoPlayer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
+import java.io.File
 
 
+@InstallIn(ServiceComponent::class)
 @Module
-@InstallIn(ServiceModule::class)
 object ServiceModule {
 
     @Provides
     @ServiceScoped
-
+//    @Singleton
     fun provideAudioAttributes(): AudioAttributes =
         AudioAttributes.Builder().setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
             .setUsage(C.USAGE_MEDIA).build()
 
+    @Provides
+    @ServiceScoped
+//    @Singleton
+    fun provideExoPlayer(
+        @ApplicationContext context: Context, audioAttributes: AudioAttributes
+    ): ExoPlayer = ExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true)
+        setHandleAudioBecomingNoisy(true)
+    }
+
 
     @Provides
     @ServiceScoped
-    fun provideExoPlayer(
-        @ApplicationContext context: Context, audioAttributes: AudioAttributes
-    ):ExoPlayer = ExoPlayer.Builder(context).build().apply {
-        setAudioAttributes(audioAttributes,true)
-        setHandleAudioBecomingNoisy(true)
+//    @Singleton
+    fun provideDataSourceFactory(@ApplicationContext context: Context) =
+        DefaultDataSource.Factory(context)
+    @Provides
+    @ServiceScoped
+//    @Singleton
+    @OptIn(UnstableApi::class)
+    fun provideCacheDataSourceFactory(
+        @ApplicationContext context: Context,
+        dataSource: DefaultDataSource.Factory
+    ) :CacheDataSource.Factory{
+        val cacheDir = File(context.cacheDir,"media")
+        val databaseProvider = StandaloneDatabaseProvider(context)
+        val cache = SimpleCache(cacheDir,NoOpCacheEvictor(),databaseProvider)
+
+        return CacheDataSource.Factory().apply {
+            setCache(cache)
+            setUpstreamDataSourceFactory(dataSource)
+        }
+
     }
+
+
+
 
 
 }
--- app/src/main/java/com/github/ebrahimi2723/music/service/MediaPlayerService.kt
+++ app/src/main/java/com/github/ebrahimi2723/music/service/MediaPlayerService.kt
@@ -3,11 +3,23 @@ package com.github.ebrahimi2723.music.service
 import android.media.browse.MediaBrowser
 import android.os.Bundle
 import android.service.media.MediaBrowserService
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.exoplayer.ExoPlayer
 import com.github.ebrahimi2723.music.constants.MyConstant
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MediaPlayerService : MediaBrowserService() {
+
+
+
+    @Inject
+    lateinit var  dataSourceFactory:CacheDataSource.Factory
+
+    @Inject
+    lateinit var exoPlayer: ExoPlayer
+
     override fun onGetRoot(clintPackageName: String, clintUid: Int, rootHints: Bundle?): BrowserRoot {
 
        return BrowserRoot(MyConstant.MEDIA_ROOT_ID,null)
